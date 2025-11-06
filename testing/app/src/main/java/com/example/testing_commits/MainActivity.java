f065af57f1c47c8534e4cb22ec3c6dd7d9076018
Kori-Kozicki <156161231+Kori-Kozicki@users.noreply.github.com>
2025-11-06 15:40:26 -0700

    Initial upload

---
+++ testing/app/src/main/java/com/example/testing_commits/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.testing_commits;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+/**
+ * This comment was before I uploaded the file. I am testing that I can actually travese through commit histories
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
